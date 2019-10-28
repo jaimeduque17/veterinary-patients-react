5e1648dbede424dfbef98ec5e56034521b9f7658
Jaime Duque <jaimeduque17@gmail.com>
2019-10-27 23:46:14 -0500

    The message of the appointment list is modified

--- src/components/AppointmentList.js
+++ src/components/AppointmentList.js
@@ -2,22 +2,28 @@ import React from 'react';
 import Appointment from './Appointment';
 import PropTypes from 'prop-types';
 
-const AppointmentList = ({ appointments, deleteAppointment }) => (
-    <div className="card mt-2 py-5">
-        <div className="card-body">
-            <h2 className="card-title text-center">Manage appointments here</h2>
-            <div className="appointment-list">
-                {appointments.map(appointment => (
-                    <Appointment 
-                        key={appointment.id}
-                        appointment={appointment}
-                        deleteAppointment={deleteAppointment}  
-                    />
-                ))}
+const AppointmentList = ({ appointments, deleteAppointment }) => {
+
+    // Print a message depending on whether there are appointments or not
+    const message = Object.keys(appointments).length === 0 ? 'No appointments' : 'Manage appointments here';
+
+    return (
+        <div className="card mt-2 py-5">
+            <div className="card-body">
+                <h2 className="card-title text-center">{message}</h2>
+                <div className="appointment-list">
+                    {appointments.map(appointment => (
+                        <Appointment 
+                            key={appointment.id}
+                            appointment={appointment}
+                            deleteAppointment={deleteAppointment}  
+                        />
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+}
 
 AppointmentList.propTypes = {
     appointments: PropTypes.array.isRequired,
